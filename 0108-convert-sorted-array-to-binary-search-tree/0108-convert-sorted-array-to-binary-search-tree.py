bee1fe78bf7107f43299b03db6e004163bc376ab
Nomaan <taranumjahan336@gmail.com>
2026-08-20 16:09:18 +0530

    Time: 0 ms (100%), Space: 20.3 MB (30.59%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def build(self,left,right,nums):
+        if left>right:
+            return
+
+        mid = (left+right)//2
+        root = TreeNode(nums[mid])
+        root.left = self.build(left,mid-1,nums) 
+        root.right = self.build(mid+1,right,nums)
+
+        return root   
+
+
+
+            
+    
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        return self.build(0,len(nums)-1,nums)
+        
