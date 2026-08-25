b209f938f1ea99633ec9f8ce8cfe2e6e7c7539c2
Nomaan <taranumjahan336@gmail.com>
2026-08-25 22:52:04 +0530

    Time: 6 ms (76.68%), Space: 19.4 MB (14.87%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        nums.sort()
+        if len(nums)<3:
+            return -1
+
+        return nums[len(nums)//2]    
+        
