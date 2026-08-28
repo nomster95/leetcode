7c9f71f03a4f89b539496c249a6f3ed3e012c0bd
Nomaan <taranumjahan336@gmail.com>
2026-08-28 12:38:11 +0530

    Time: 0 ms (100%), Space: 20.7 MB (82.11%) - LeetHub

---
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrangeWords(self, text: str) -> str:
+        words = text.split()
+        words[0] = words[0].lower()
+        words.sort(key = len)
+        words[0] = words[0].capitalize()
+        m = " ".join(words)
+        return m
+        
