1e13ae65240365bd3afc575189967384f65a7a70
Nomaan <taranumjahan336@gmail.com>
2026-09-05 15:43:00 +0530

    Time: 3 ms (36.02%), Space: 19 MB (98.11%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        odd = 0
+        even = 0
+        for i in range(len(num)):
+            if i%2==0:
+                even+=int(num[i])
+            else:
+                odd+=int(num[i])
+
+        if odd==even:
+            return True
+
+        return False                
+
+        
