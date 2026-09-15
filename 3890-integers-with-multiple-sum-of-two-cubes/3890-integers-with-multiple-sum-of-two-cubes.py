c27192e39fa2235a765167ad87e7ee37040dc6d8
Nomaan <taranumjahan336@gmail.com>
2026-09-15 18:29:05 +0530

    Time: 485 ms (68.18%), Space: 64.3 MB (83.12%) - LeetHub

--- 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.py
+++ 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.py
@@ -1,27 +1,21 @@
 class Solution:
     def findGoodIntegers(self, n: int) -> list[int]:
-        freq = {}
-        ans = []
-        limit = int(n**(1/3))+1
+        seen = set()
+        ans = set()
+
+        limit = int(n ** (1 / 3)) + 1
+
         for a in range(limit):
-            for b in range(a,limit):
-                total = a**3+b**3
-                if total>n:
+            for b in range(a, limit):
+                total = a**3 + b**3
+
+                if total > n:
                     break
 
-                if total not in freq:
-                    freq[total] = 1
+                if total in seen:
+                    ans.add(total)
                 else:
-                    freq[total]+=1
+                    seen.add(total)
 
-        for i in freq:
-            if freq[i]>=2:
-                ans.append(i)
-
-        ans.sort()        
-
-        return ans                    
-
-
-
+        return sorted(ans)
         
