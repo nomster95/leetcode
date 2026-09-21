3fc6178ece5409f98e018911b3d903306b833c7c
Nomaan <taranumjahan336@gmail.com>
2026-09-21 18:40:31 +0530

    Time: 67 ms (8.84%), Space: 33.6 MB (67.87%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        freq = {}
+        ans = []
+        ops = 0
+        length = 0
+        for i in nums:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i]+=1
+
+
+        for j in freq:
+            if freq[j]==1:
+                return -1
+            elif freq[j]%3==0:
+                ops+=freq[j]//3
+            elif freq[j]%3==1:
+                freq[j]-=4
+                ops+=2
+                ops+=freq[j]//3
+            elif freq[j]%3==2:
+                freq[j]-=2
+                ops+=1
+                ops+=freq[j]//3
+            
+
+                
+
+
+        return ops   
+
+
