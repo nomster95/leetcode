03a7427b5df530b7f10d6691af242d4bd4dec3ec
Nomaan <taranumjahan336@gmail.com>
2026-09-23 21:14:39 +0530

    Time: 1503 ms (90.24%), Space: 19.3 MB (76.7%) - LeetHub

---
+++ 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countRatioSubarrays(self, nums: list[int], a: int, b: int) -> int:
+        valid = 0
+        for i in range(len(nums)):
+            x = 0
+            y = 0
+            for j in range(i,len(nums)):
+                if nums[j]%2==0:
+                    x+=1
+                else:
+                    y+=1
+
+                if y>0 and b*x<=a*y:
+                    valid+=1
+
+        return valid            
+
+
+        
