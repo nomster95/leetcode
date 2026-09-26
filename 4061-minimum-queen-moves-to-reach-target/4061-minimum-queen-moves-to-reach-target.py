576d4e99519dd956fb38504a1109e611276eaef3
Nomaan <taranumjahan336@gmail.com>
2026-09-26 23:13:16 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.33%) - LeetHub

--- 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.py
+++ 4061-minimum-queen-moves-to-reach-target/4061-minimum-queen-moves-to-reach-target.py
@@ -3,9 +3,8 @@ class Solution:
 
         if source==target:
             return 0
-        elif source[0]==target[0] or source[1]==target[1] or source[0]+source[1]==target[0]+target[1] or source[0]-source[1]==target[0]-target[1]:
+        elif source[0]==target[0] or source[1]==target[1] or abs(source[0]-target[0])==abs(source[1]-target[1]):
             
-
             return 1
         else:
             return 2        
